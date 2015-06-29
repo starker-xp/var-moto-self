054251ae26d7b54fe903ccc33abc404e308bb307
Guillaume <guillaume.caouissin@gmail.com>
2015-06-29 13:08:29 +0200

    Ajout du controller recherche + route Refactorisation des templates

---
+++ src/Starkerxp/EcommerceBundle/Controller/RecherchesController.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace Starkerxp\EcommerceBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class RecherchesController extends Controller
+{
+
+    public function indexAction()
+    {
+
+    }
+
+}
