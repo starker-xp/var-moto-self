cc7c13db5de11db31a461e0fb79d03377144cf71
Guillaume <guillaume.caouissin@gmail.com>
2015-06-30 12:29:45 +0200

    Refactorisation du systeme de generation de module CQRSES

--- src/Starkerxp/CQRSESBundle/Command/GenererBaseCQRSCommand.php
+++ src/Starkerxp/CQRSESBundle/Command/GenererBaseCQRSCommand.php
@@ -2,6 +2,7 @@
 
 namespace Starkerxp\CQRSESBundle\Command;
 
+use Starkerxp\CQRSESBundle\Generator\CQRSESGenerator;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
@@ -24,89 +25,21 @@ class GenererBaseCQRSCommand extends ContainerAwareCommand
         $nomBundle = $input->getArgument('bundle');
         $nomDomain = ucfirst($input->getArgument('domain'));
 
-// On recupere les infos sur le bundle nécessaire à la génération du controller
+        // On recupere les infos sur le bundle nécessaire à la génération du controller
         $kernel = $this->getContainer()->get('kernel');
         $bundle = $kernel->getBundle($nomBundle);
         $namespace = $bundle->getNamespace();
-        $namespaceFQC = str_replace("\\", "\\\\", $namespace);
         $path = $bundle->getPath();
-        // On interdit un namespace en particulier.
-        if ($namespace == "Starkerxp\CQRSESBundle") {
-            $output->writeln("Impossible d'utiliser ce namespace. Créer un nouveau bundle.");
-            exit;
-        }
-        $repertoireGenere = $this->genererRepertoires($nomDomain, $path);
-        if (!$repertoireGenere) {
-            $output->writeln("Impossible d'utiliser ce domaine. Il est déjà utilisé dans ce bundle.");
-            exit;
-        }
-        exit;
-        $this->genererFichiers($nomDomain, $path, $namespace, $namespaceFQC);
-    }
 
-    public function genererRepertoires($nomDomain, $path)
-    {
-        $repertoiresStructures = [
-            'Ressources/config/',
-            'Services/Command/_nomDomaine_/',
-            'Services/Domain/_nomDomaine_/Event/',
-            'Services/Persistence/Lecture/',
-            'Services/Persistence/Ecriture/_nomDomaine_/',
-            'Services/Query/_nomDomaine_/',
-        ];
-        // On génère la liste des répertoires qui seront utilisés.
-        foreach ($repertoiresStructures as $repertoire) {
-            // On sauvegarde pour la génrétation des fichiers.
-            $repertoireRenomme = str_replace("_nomDomaine_", $nomDomain, $repertoire);
-            // On génère les dossiers nécessaires.
-            $target = $path . '/' . $repertoireRenomme;
-            if (is_dir($target) && preg_match("_nomDomaine_", $repertoire)) {
-                return false;
-            }
-            if (!is_dir($target)) {
-                mkdir($target, 0777, true);
-            }
-        }
-        return true;
+        $bundleCQRSES = $kernel->getBundle("StarkerxpCQRSESBundle");
+        $generator = new CQRSESGenerator();
+        $generator->setSkeletonDirs($bundleCQRSES->getPath() . '/Resources/views/');
+        $generator->generate($namespace, $path . '/Services/', $nomDomain);
     }
 
     public function genererFichiers($nomDomain, $path, $namespace, $namespaceFQC)
     {
-        $fichiersStructures = [
-            'Ressources/config/' => [
-                "services._lnomDomaine_.yml"
-            ],
-            'Services/Command/_nomDomaine_/' => [
-                'Creer_nomDomaine_Command.php',
-                'Creer_nomDomaine_Handler.php',
-                'Modifier_nomDomaine_Command.php',
-                'Modifier_nomDomaine_Handler.php',
-                'Supprimer_nomDomaine_Command.php',
-                'Supprimer_nomDomaine_Handler.php',
-            ],
-            'Services/Domain/_nomDomaine_/' => [
-                '_nomDomaine_Collection.php',
-                '_nomDomaine_DTO.php',
-                '_nomDomaine_POPO.php',
-                '_nomDomaine_Domain.php',
-                'Event/_nomDomaine_AEteCree.php',
-                'Event/_nomDomaine_AEteModifie.php',
-                'Event/_nomDomaine_AEteSupprime.php',
-            ],
-            'Services/Persistence/Ecriture/_nomDomaine_/' => [
-                '_nomDomaine_Projection.php',
-                '_nomDomaine_Repository.php',
-            ],
-            'Services/Persistence/Lecture/' => [
-                '_nomDomaine_Repository.php',
-            ],
-            'Services/Query/_nomDomaine_/' => [
-                '_nomDomaine_ListerQuery.php',
-                '_nomDomaine_ListerQueryHandler.php',
-                '_nomDomaine_Query.php',
-                '_nomDomaine_QueryHandler.php',
-            ],
-        ];
+
         // On récupère le moteur de template twig
         $twig = $this->getContainer()->get('templating');
 
---
+++ src/Starkerxp/CQRSESBundle/Generator/CQRSESGenerator.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace Starkerxp\CQRSESBundle\Generator;
+
+use Sensio\Bundle\GeneratorBundle\Generator\Generator;
+
+class CQRSESGenerator extends Generator
+{
+
+    public function generate($namespace, $dir, $domain)
+    {
+        if (file_exists($dir)) {
+            if (!is_dir($dir)) {
+                throw new \RuntimeException(sprintf('Unable to generate the bundle as the target directory "%s" exists but is a file.', realpath($dir)));
+            }
+            $files = scandir($dir);
+            if ($files != array('.', '..')) {
+                throw new \RuntimeException(sprintf('Unable to generate the bundle as the target directory "%s" is not empty.', realpath($dir)));
+            }
+            if (!is_writable($dir)) {
+                throw new \RuntimeException(sprintf('Unable to generate the bundle as the target directory "%s" is not writable.', realpath($dir)));
+            }
+        }
+
+        $parameters = array(
+            'nomDeDomaine' => $domain,
+            'nomDeDomaineMinuscule' => lcfirst($domain),
+            'namespace' => $namespace,
+            'namespaceFQC' => str_replace("\\", "\\\\", $namespace),
+        );
+        $fichiers = $this->getFichiers();
+
+        foreach ($fichiers as $fichier) {
+            $twigTemplate = "Services/" . $fichier . ".twig";
+            $twigTemplate = str_replace("Services/../", "", $target);
+
+            $target = $dir . str_replace(['_nomDomaine_', '_lnomDomaine_'], [$domain, lcfirst($domain)], $fichier);
+            $target = str_replace("Services/../", "", $target);
+            $this->renderFile($twigTemplate, $target, $parameters);
+        }
+    }
+
+    public function getFichiers()
+    {
+        return [
+            '../Resources/config/services._lnomDomaine_.yml',
+            'Command/_nomDomaine_/Creer_nomDomaine_Command.php',
+            'Command/_nomDomaine_/Creer_nomDomaine_Handler.php',
+            'Command/_nomDomaine_/Modifier_nomDomaine_Command.php',
+            'Command/_nomDomaine_/Modifier_nomDomaine_Handler.php',
+            'Command/_nomDomaine_/Supprimer_nomDomaine_Command.php',
+            'Command/_nomDomaine_/Supprimer_nomDomaine_Handler.php',
+            'Domain/_nomDomaine_/_nomDomaine_Collection.php',
+            'Domain/_nomDomaine_/_nomDomaine_DTO.php',
+            'Domain/_nomDomaine_/_nomDomaine_POPO.php',
+            'Domain/_nomDomaine_/_nomDomaine_Domain.php',
+            'Domain/_nomDomaine_/Event/_nomDomaine_AEteCree.php',
+            'Domain/_nomDomaine_/Event/_nomDomaine_AEteModifie.php',
+            'Domain/_nomDomaine_/Event/_nomDomaine_AEteSupprime.php',
+            'Persistence/Ecriture/_nomDomaine_/_nomDomaine_Projection.php',
+            'Persistence/Ecriture/_nomDomaine_/_nomDomaine_Repository.php',
+            'Persistence/Lecture/_nomDomaine_Repository.php',
+            'Query/_nomDomaine_/_nomDomaine_ListerQuery.php',
+            'Query/_nomDomaine_/_nomDomaine_ListerQueryHandler.php',
+            'Query/_nomDomaine_/_nomDomaine_Query.php',
+            'Query/_nomDomaine_/_nomDomaine_QueryHandler.php',
+        ];
+    }
+
+}
